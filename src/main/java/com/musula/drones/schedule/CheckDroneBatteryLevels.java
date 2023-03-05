2d36f20393aea9f57cd07446a315bb10f5d3dacb
nischalshakya15 <nischalshakya95@gmail.com>
2023-03-05 10:04:39 +0545

    24-Configure log and schedule task to check the battery level.

---
+++ src/main/java/com/musula/drones/schedule/CheckDroneBatteryLevels.java
@@ -0,0 +1,34 @@
+package com.musula.drones.schedule;
+
+import com.musula.drones.domain.drone.entity.Drone;
+import com.musula.drones.domain.drone.repository.DroneRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.List;
+
+@EnableAsync
+@Configuration
+@EnableScheduling
+@AllArgsConstructor
+@Slf4j
+public class CheckDroneBatteryLevels {
+
+  private final DroneRepository droneRepository;
+
+  @Scheduled(fixedRate = 5000)
+  public void scheduleBatteryLevelCheck() throws InterruptedException {
+    List<Drone> drones = droneRepository.findAll();
+    for (Drone drone : drones) {
+      log.info(
+          "Serial Number: {} and Battery Level: {}",
+          drone.getSerialNumber(),
+          drone.getBatteryPercentage());
+    }
+    Thread.sleep(5000);
+  }
+}
